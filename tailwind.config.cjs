0a2cec88ceef1dbb05a0e876a18d84adac52c48f
Zara <Zara>
2024-07-20 14:47:51 +0200

    yuh

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-    plugins: [
-        require('@tailwindcss/typography'),
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/aspect-ratio'),
-        require('@tailwindcss/container-queries'),
-    ],
-  };
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms"), require("@tailwindcss/aspect-ratio"), require("@tailwindcss/container-queries")],
+  theme: {
+    extend: {
+      colors: {
+        coral: "#FF6F61",
+        mintgreen: "#CCFFD6",
+        blue: "#6BB5FF",
+      },
+      fontFamily: {
+        scree: ["Sree Krushnadevaraya"],
+        sans: ["Open Sans"],
+      },
+    },
+  },
+};
